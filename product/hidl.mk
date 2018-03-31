a3d8b69e1c95bcdb199202ae12d4aebd9bd9903c
karthikkm007 <jacksparrow941994@gmail.com>
2018-03-31 14:49:01 +0530

    wt88047:manually reverting hidl.mk

--- product/hidl.mk
+++ product/hidl.mk
@@ -1,7 +1,6 @@
 # HIDL
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml \
-    $(LOCAL_PATH)/compatibility_matrix.xml:system/vendor/compatibility_matrix.xml
+    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
 
 # Temp disable media treble until i fix OMX decoder for inapp players
 PRODUCT_PROPERTY_OVERRIDES += \
