d7184d3c61a628e1479d0bc29efc3f212eda07a7
karthikkm007 <jacksparrow941994@gmail.com>
2018-04-06 18:57:59 +0530

    wt88047:treble WIP

--- product/media.mk
+++ product/media.mk
@@ -1,4 +1,4 @@
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$vendor/etc/media_profiles_V1_0.xml \
-    $(LOCAL_PATH)/configs/media_codecs_performance.xml:$vendor/etc/media_codecs_performance.xml
+    $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:vendor/etc/media_profiles_V1_0.xml \
+    $(LOCAL_PATH)/configs/media_codecs_performance.xml:vendor/etc/media_codecs_performance.xml
